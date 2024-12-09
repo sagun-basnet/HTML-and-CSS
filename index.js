c77086b9377d5b6c9eda7729e08946a1a512bb5d
sagun basnet <basnetsagun8@gmail.com>
2024-12-09 12:23:56 +0545

    asdf

---
+++ index.js
@@ -0,0 +1,100 @@
+//Defining variable
+// var a = "a";
+// let b = "A";
+// const c = 9;
+
+for (let i = 0; i < 10; i++) {
+  //   console.log("hello");
+}
+
+// while (true) {
+
+// }
+
+// do{
+
+// }while();
+
+// if(a || b){
+//     console.log("Yes");
+
+// }else{
+//     console.log("NO");
+
+// }
+
+// switch (a) {
+//     case 'a':
+
+// }
+
+let arry = [1, 2, "hello", [1, 2, 3, 4], true];
+
+// console.log(arry[2], 33);
+
+let obj = {
+  name: "sagar",
+  age: 12,
+  address: {
+    street: "street1",
+    city: "city1",
+    country: "",
+  },
+};
+
+// console.log(obj.address, 45);
+
+delete obj.address;
+// console.log(obj, 48);
+
+// console.log(obj.name, obj.age, obj.address.street);
+
+// Function =================>
+// function add(a, b, c) {
+//   console.log(a + b + c, "addition");
+// }
+// add();
+
+const add = (a, b, c) => {
+  console.log(a + b + c, "addition");
+};
+
+add(3, 5, 6);
+
+// Array methods
+let arr = [1, 4, 2, 6, 7, 8, 9, 4];
+
+let n = arr.indexOf(4);
+
+// console.log(n, "n");
+
+// arr.push(10);
+
+// console.log(arr.pop());
+
+// console.log(arr, "after");
+
+// arr.shift();
+
+// arr.unshift(5);
+
+// arr.splice(1,2)
+
+// console.log(arr, "before");
+
+const newArr = arr.slice(1, 3);
+
+// console.log(newArr, "after");
+
+const btn = document.getElementById("btn");
+const btn2 = document.getElementsByClassName("btn");
+const btn3 = document.getElementsByTagName("button");
+const btn4 = document.querySelector(".btn");
+const btn5 = document.querySelectorAll(".btn");
+
+console.log(btn);
+
+const btnClick = () => {
+  btn.style.backgroundColor = "blue";
+  alert(btn);
+};
